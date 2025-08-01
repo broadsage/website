2e57cf6ecc85c07d46c353e310aa331e1523b197
Jeh1928 <jbanimineni@broadsage.com>
2025-08-01 18:16:37 +0530

    feat: observability and analytics packages added
    Signed-off-by: Jeh1928 <jbanimineni@broadsage.com>

--- packages/design-system/lib/utils.ts
+++ packages/design-system/lib/utils.ts
@@ -1,3 +1,4 @@
+import { parseError } from '@repo/observability/error';
 import { clsx } from 'clsx';
 import type { ClassValue } from 'clsx';
 import { toast } from 'sonner';
@@ -9,20 +10,7 @@ export const capitalize = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1);
 
 export const handleError = (error: unknown): void => {
-  let message = 'An unknown error occurred.';
-  let stack = '';
-  if (error instanceof Error) {
-    message = error.message;
-    stack = error.stack || '';
-  } else if (typeof error === 'string') {
-    message = error;
-  }
-  // Log error message and stack trace
-  // eslint-disable-next-line no-console
-  console.error('Error:', message);
-  if (stack) {
-    // eslint-disable-next-line no-console
-    console.error('Stack trace:', stack);
-  }
+  const message = parseError(error);
+
   toast.error(message);
 };
